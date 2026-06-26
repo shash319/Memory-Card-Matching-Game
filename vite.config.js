c6328de071e993548c50be2043d5d1bde1231994
Shasawat Pal <shashwatpal2001@gmail.com>
2026-06-26 22:18:01 +0530

    new commit message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/Memory-Card-Matching-Game/",
-});
+  base: mode === "github" ? "/Memory-Card-Matching-Game/" : "/",
+}));
